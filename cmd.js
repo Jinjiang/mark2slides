62076ba493139d05047f5d647d9110f06f55ab77
Jinjiang <zhaojinjiang@me.com>
2019-04-02 07:20:42 +0800

    supported build & clean

--- cmd.js
+++ cmd.js
@@ -1,12 +1,30 @@
 #!/usr/bin/env node
 
-// todo: dev, clean
-const argv = require('minimist')(process.argv.slice(2))
-const { msg } = require('./msg')
+const minimist = require('minimist')
+const { msg, clean } = require('./msg')
 
-const input = argv.i || argv.input || '.'
-const output = argv.o || argv.output || '_dist'
+const argv = minimist(process.argv.slice(2))
+// console.log(argv)
 
-console.log(argv)
+if (argv._.length > 1) {
+  console.error(`Sorry, you couldn't run more than one commands at the same time.`)
+  return;
+}
 
-msg(input, output)
+const cmd = argv._[0]
+
+switch (cmd) {
+  case 'clean':
+    // console.log('clean()')
+    clean()
+    break
+  case undefined:
+  case 'build':
+    const input = argv.i || argv.input || '.'
+    const output = argv.o || argv.output || 'dist'
+    // console.log('msg(input, output)')
+    msg(input, output)
+    break
+  default:
+    console.error(`Sorry, command "${cmd}" not supported.`)
+}
