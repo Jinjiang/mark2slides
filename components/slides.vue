9da734461f694bdd033869bbbd6de19d811eb9b5
Jinjiang <zhaojinjiang@me.com>
2019-04-01 11:55:08 +0800

    supported importing markdown files

---
+++ components/slides.vue
@@ -0,0 +1,16 @@
+<template>
+  <MarkDisplay :markdown="content" autoBaseUrl autoBlankTarget autoFontSize keyboardCtrl urlHashCtrl supportPreview />
+</template>
+
+<script>
+import MarkDisplay from 'vue-mark-display';
+
+export default {
+  components: {
+    MarkDisplay
+  },
+  props: {
+    content: String
+  }
+}
+</script>
